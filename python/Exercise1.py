f6c7f4719e9fd16ed1bb3e58edeb91dd513ef7cd
Lee Stott <leestott@microsoft.com>
2019-02-21 12:44:25 +0000

    cleanup

--- python/Exercise1.py
+++ python/Exercise1.py
@@ -9,23 +9,23 @@ except:
 # # Exercise01 : Prepare Config Settings
 # 
 # Set your config in your current project folder.    
-# (Before starting, you must create your Machine Learning service workspace with Azure Portal. See [Readme](https://github.com/tsmatz/azure-ml-tensorflow-complete-sample/).)
+# (Before starting, you must create your Machine Learning service workspace with Azure Portal. See [Readme](/Readme.md).)
 # 
-# *back to [index](https://github.com/tsmatz/azure-ml-tensorflow-complete-sample/)*
+# *back to [index](/Readme.md)*
 #%% [markdown]
 # ## Create config settings
 # 
 # You must create config setting into your project directory. The result was written in ```aml_config/config.json```.    
 # If you're asked to login Azure with device login UI (https://microsoft.com/devicelogin), please open your browser and proceed to login.
 # 
-# This is needed **only once** in your project directory.
+# This is needed **only once** in your project directory. edit the notebook cells below to enter your resource_group, workspace_name and subscription_id
 
 #%%
 from azureml.core import Workspace
 ws = Workspace(
-  workspace_name = "mlws01",
-  subscription_id = "b3ae1c15-...",
-  resource_group = "TestGroup01")
+  workspace_name = "your AzureML WorkSpace",
+  subscription_id = "your subscriptionID",
+  resource_group = "your resource_group")
 ws.write_config()
 
 #%% [markdown]
